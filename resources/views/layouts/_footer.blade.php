979560c78aa46d955ce11a474cc4b6c376c27406
hasee-linux <larawsw@outlook.com>
2020-06-01 11:14:54 +0800

    feat: update footer text

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer">
     <img class="brand-icon" src="https://cdn.kuaidi100.com/images/favicon.gif">
-    <a href="https://m.kuaidi100.com/" target="_blank">快递查询</a>
+    <a href="https://m.kuaidi100.com/" target="_blank">快递100</a>
     {{--    <a href="https://learnku.com/laravel/courses" target=_blank>--}}
     {{--        刻意练习，每日精进--}}
     {{--    </a>--}}
