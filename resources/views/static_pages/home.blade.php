5172732d1fb2eec0762d29b06deb625d3ee4bafe
hasee-linux <larawsw@outlook.com>
2020-06-01 16:41:21 +0800

    feat: update view文案

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -22,9 +22,9 @@
         </div>
     @else
         <div class="jumbotron">
-            <h1>Hello Laravel</h1>
+            <h1>Hello World</h1>
             <p class="lead">
-                你现在所看到的是 <a href="https://learnku.com/courses/laravel-essential-training">Laravel 入门教程</a> 的示例项目主页。
+                欢迎来到Larry的生活信息查询项目
             </p>
             <p>
                 一切，将从这里开始。
