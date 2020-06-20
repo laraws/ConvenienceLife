903aab52dfc4c6674088e80353af7261ebe4ad9d
hasee-linux <larawsw@outlook.com>
2020-06-20 17:39:46 +0800

    feat: move icp style to scss

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <img class="brand-icon" src="https://cdn.kuaidi100.com/images/favicon.gif">
     <a href="https://m.kuaidi100.com/" target="_blank">快递100</a>
-    <a href="http://www.beian.miit.gov.cn/" style="display: inline-block; float: right">鄂ICP备18008740号-1</a>
+    <a href="http://www.beian.miit.gov.cn/" class="icp">鄂ICP备18008740号-1</a>
     {{--    <a href="https://learnku.com/laravel/courses" target=_blank>--}}
     {{--        刻意练习，每日精进--}}
     {{--    </a>--}}
